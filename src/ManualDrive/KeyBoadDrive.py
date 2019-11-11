4ef09247df408fcd51baa48b0430f278188a4849
ishu59 <ashish.jha59@gmail.com>
2019-11-11 09:02:34 -0500

    Adding changes for local driving

--- src/ManualDrive/KeyBoadDrive.py
+++ src/ManualDrive/KeyBoadDrive.py
@@ -2,14 +2,15 @@ import os,sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
-from Bot.BasicBot import BasicBot
+# from Bot.BasicBot import BasicBot
+from Bot.BasicBotLocal import BasicBotLocal
 from pynput.keyboard import Key, Listener
 bot_ip_address = '10.0.0.195'
-os.environ["PIGPIO_ADDR"] = bot_ip_address
-os.environ["GPIOZERO_PIN_FACTORY"] = "pigpio"
+# os.environ["PIGPIO_ADDR"] = bot_ip_address
+# os.environ["GPIOZERO_PIN_FACTORY"] = "pigpio"
 
-bot = BasicBot(host=bot_ip_address)
-
+# bot = BasicBot(host=bot_ip_address)
+bot = BasicBotLocal()
 def on_press(key):
     print('{0} pressed'.format(key))
     if key == 'w' or key == Key.up:
